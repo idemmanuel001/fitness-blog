b94002559a73c8cbca64147e8883edefc934ded0
idemmanuel001 <idemmanuel001@gmail.com>
2022-10-18 03:49:20 +0100

    .

--- components/AuthorPreview.jsx
+++ components/AuthorPreview.jsx
@@ -3,14 +3,13 @@ import Image from 'next/image';
 const AuthorPreview = ({ author }) => {
   const { name, thumbnail, bioPreview } = author;
 
-  console.log(author);
   return (
     <div className='flex flex-col items-center w-full md:items-start'>
       <h2 className="mb-2 font-bold text-raisin-black">
         About Author
       </h2>
 
-      <div className='relative block w-4/5 max-w-2xl border h-52 md:h-40 '>
+      <div className='relative block w-11/12 max-w-2xl border h-52 md:h-40 '>
         <Image
           src={thumbnail.url}
           alt={thumbnail.title}
@@ -21,7 +20,7 @@ const AuthorPreview = ({ author }) => {
         />
       </div>
 
-      <p className="w-4/5 my-2 text-sm text-justify text-medium-black md:text-left">
+      <p className="w-11/12 my-2 text-sm text-justify text-medium-black md:text-left">
         {bioPreview}
       </p>
     </div>
--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,20 +1,69 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+
 
 const Footer = () => {
-  return (
-    <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/images/logo.svg" alt="Logo" width={72} height={16} />
-        </a>
-      </footer>
-  )
-}
 
-export default Footer
+  return (
+    <footer className="w-full h-fit">
+      <div className="flex flex-col justify-center items-center px-4 py-6 w-full bg-clear-day">
+        <div className="w-11/12 h-full max-w-lg flex flex-col justify-center items-center">
+          <h2 className="mb-2 font-bold text-center text-raisin-black">
+            SUBSCRIBE AND DON'T MISS NEW ARTICLES
+          </h2>
+
+          <p className="text-center text-sm w-11/12">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque sint
+            quo nemo pariatur
+            expedita nulla maiores tempore, perspiciatis debitis. Similique.
+          </p>
+        </div>
+
+        {/* subscribtion form */}
+
+        <div className="flex items-center w-full justify-center">
+
+          <form 
+          className='flex flex-col w-full max-w-lg  md:flex-row items-center justify-center my-6 '
+          >
+            <div className="flex flex-col w-11/12 mb-3 md:mb-0 items-start md:mr-4 ">
+              <input 
+                className='bg-white md:w-32 w-full text-dark-gray p-3 text-sm  md:py-1 md:px-1.5 shadow-inner'
+                type="text"
+                name="name"
+                width={20}
+                id="name"
+                placeholder="Name"
+                required />
+            </div>
+            <div className="flex flex-col w-11/12 mb-3 md:mb-0 items-start md:mr-4 ">
+              <input
+                className='bg-white w-full md:w-52 text-dark-gray p-3 md:py-1 md:px-1.5 text-sm shadow-inner'
+                type="email"
+                name="email"
+                width={40}
+                id="email"
+                placeholder="Email"
+                required />
+            </div>
+
+            <button 
+            className='md:py-1 p-3 md:px-3 w-11/12 bg-dark-green text-white text-sm font-semibold '
+            type="submit">
+              SEND
+            </button>
+          </form>
+        </div>
+
+        
+      </div>
+      <div className="flex justify-center items-center h-fit py-4 bg-raisin-black text-lavender ">
+         <p className="text-center text-sm py-4 font-semibold md:px-4">
+          Built by Idoko Emmanuel, 2022. All rights reserved.
+         </p>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
