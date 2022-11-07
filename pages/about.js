8d6de16d03ec9f7ff03350b7cc2e73347ccf09e6
idemmanuel001 <idemmanuel001@gmail.com>
2022-11-07 17:28:00 +0100

    added more styles to the about page

--- pages/about.js
+++ pages/about.js
@@ -7,25 +7,27 @@ const about = ({ author }) => {
     console.log(author);
 
     return (
-        <div>
-            <div>
-                <h1>HI! I AM {author.name}, AND I'M GLAD TO SEE YOU HERE</h1>
-                <p>{author.bioPreview}</p>
-            </div>
-
-            <div>
-                <div className='relative aspect-[6/9]'>
-                    <Image
-                        src={author.thumbnail.url}
-                        alt={author.thumbnail.title}
-                        layout='fill'
-                        quality={30}
-                        objectFit='cover'
-                        objectPosition='center center'
-                    />
+        <div className='w-full py-3 px-6'>
+            <div className='flex w-full max-w-4xl mx-auto flex-col md:flex-row '>
+                <div className='w-full max-w-2xl mb-2'>
+                    <h1 className='my-3 text-xl uppercase font-bold md:mb-6 md:text-3xl'>HI! I AM {author.name}, AND I'M GLAD TO SEE YOU HERE</h1>
+                    <p>{author.bioPreview}</p>
                 </div>
 
-                <SocialMedia author={author} />
+                <div className='my-2 w- md:ml-10 max-w-lg'>
+                    <div className='my-1 relative aspect-[6/9] md:max-w-md md:w-full md:h-56'>
+                        <Image
+                            src={author.thumbnail.url}
+                            alt={author.thumbnail.title}
+                            layout='fill'
+                            quality={30}
+                            objectFit='cover'
+                            objectPosition='center center'
+                        />
+                    </div>
+
+                    <SocialMedia author={author} />
+                </div>
             </div>
 
         </div>
