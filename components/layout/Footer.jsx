b77db9b8e45ae31db4caa0a4c28d71031d944f10
idemmanuel001 <idemmanuel001@gmail.com>
2022-11-13 15:36:07 +0100

    added form validation to contact me page form

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import {MdOutlineMarkEmailRead} from 'react-icons/md'
+import { MdOutlineMarkEmailRead } from 'react-icons/md';
 
 const Footer = () => {
   const [name, setName] = useState('');
@@ -8,7 +8,7 @@ const Footer = () => {
   const [emailError, setEmailError] = useState(false);
   const [success, setSuccess] = useState(false);
 
-
+  /* valid email regex pattern */
   const emailRegex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
 
   const handleNamechange = (event) => {
@@ -26,7 +26,7 @@ const Footer = () => {
     if (!email) setEmailError(true);
     if (emailRegex.test(email) === false) setEmailError(true);
 
-    if(!nameError && !emailError && emailRegex.test(email)) setSuccess(true)
+    if (!nameError && !emailError && emailRegex.test(email)) setSuccess(true);
     setName('');
     setEmail('');
   };
@@ -36,41 +36,41 @@ const Footer = () => {
   const emailErrorStyles = `text-red-500 px-3 my-1 md:p-1 text-xs ${emailError ? ' block ' : 'hidden'}`;
 
   /* Form successfully submited message styles */
-  const successMessageStyles = `text-center text-dark-green font-semibold w-full text-sm max-w-lg flex flex-col items-center justify-center md:flex-row mt-2 ${success ? 'block' : 'hidden'}`
+  const successMessageStyles = `text-center text-dark-green font-semibold w-full text-sm max-w-lg flex flex-col items-center justify-center md:flex-row mt-2 ${success ? 'block' : 'hidden'}`;
 
 
-  useEffect(()=>{
-   if(success){
-     setTimeout(()=> setSuccess(false), 2500)
-   }
-  })
+  useEffect(() => {
+    if (success) {
+      setTimeout(() => setSuccess(false), 2500);
+    }
+  });
 
 
   return (
     <footer className="w-full h-fit">
-      <div className="flex flex-col justify-center items-center px-6 py-6 w-full bg-clear-day">
-        <div className="w-full h-full max-w-lg flex flex-col justify-center mb-4 items-center">
-          <h2 className="mb-2 font-bold text-center w-full text-raisin-black">
+      <div className="flex flex-col items-center justify-center w-full px-6 py-6 bg-clear-day">
+        <div className="flex flex-col items-center justify-center w-full h-full max-w-lg mb-4">
+          <h2 className="w-full mb-2 font-bold text-center text-raisin-black">
             SUBSCRIBE AND DON'T MISS NEW ARTICLES
           </h2>
 
-          <p className="text-center text-sm full">
+          <p className="text-sm text-center full">
             Please sign up for my email notification so you don't miss out when I post a new article. You may withdraw your consent any time by unsubscribing.
           </p>
         </div>
 
         {/* email list form */}
-        <div className="flex flex-col items-center w-full justify-center">
+        <div className="flex flex-col items-center justify-center w-full">
 
           {/* successful form submission message */}
           <p className={successMessageStyles}>
-          <MdOutlineMarkEmailRead className='w-8 h-8 md:mr-1' />Submited successfully, Thanks for subscribing.
+            <MdOutlineMarkEmailRead className='w-8 h-8 md:mr-1' />Submited successfully, Thanks for subscribing.
           </p>
 
           <form
-            className='flex flex-col w-full max-w-lg  md:flex-row items-center md:items-start justify-center mb-6 mt-2 '
+            className='flex flex-col items-center justify-center w-full max-w-lg mt-2 mb-6 md:flex-row md:items-start '
           >
-            <div className="flex flex-col w-full md:w-32 mb-3 md:mb-0 items-start md:mr-4 ">
+            <div className="flex flex-col items-start w-full mb-3 md:w-32 md:mb-0 md:mr-4 ">
               <label className='sr-only' htmlFor="name"></label>
               <input
                 className='bg-white  w-full text-dark-gray p-3 text-sm  md:py-1 md:px-1.5 shadow-inner'
@@ -85,7 +85,7 @@ const Footer = () => {
               <p className={nameErrorStyles}>please enter a name</p>
 
             </div>
-            <div className="flex flex-col md:w-52 w-full mb-3 md:mb-0 items-start md:mr-4 ">
+            <div className="flex flex-col items-start w-full mb-3 md:w-52 md:mb-0 md:mr-4 ">
               <label className='sr-only' htmlFor="email"></label>
               <input
                 className='bg-white w-full  text-dark-gray p-3 md:py-1 md:px-1.5 text-sm shadow-inner'
@@ -101,7 +101,7 @@ const Footer = () => {
             </div>
 
             <button
-              className='md:py-1 p-3 transition duration-300 hover:text-raisin-black md:px-3 w-full md:w-28 bg-dark-green text-white text-sm font-semibold '
+              className='w-full p-3 text-sm font-semibold text-white transition duration-300 cursor-pointer md:py-1 hover:text-raisin-black md:px-3 md:w-28 bg-dark-green '
               onClick={(event) => formSubmitHandler(event)}
               type="submit">
               SEND
@@ -111,8 +111,8 @@ const Footer = () => {
 
 
       </div>
-      <div className="flex justify-center items-center h-fit py-4 bg-raisin-black text-lavender ">
-        <p className="text-center max-w-lg w-full text-sm py-4 font-semibold px-6 ">
+      <div className="flex items-center justify-center py-4 h-fit bg-raisin-black text-lavender ">
+        <p className="w-full max-w-lg px-6 py-4 text-sm font-semibold text-center ">
           Built by Idoko Emmanuel, 2022. All rights reserved.
         </p>
       </div>
--- pages/contact.js
+++ pages/contact.js
@@ -1,58 +1,151 @@
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import flowerPhoto from '../public/images/flower-photo.webp';
 import { getAuthour } from '../lib/fetchData';
 import SocialMedia from '../components/SocialMedia';
+import { MdOutlineMarkEmailRead } from 'react-icons/md';
 
 
-const contact = ({author}) => {
+
+const contact = ({ author }) => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [genericErrorMsg, setGenericErrorMsg] = useState(false);
+    const [nameError, setNameError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [messageError, setMessageError] = useState(false);
+    const [success, setSuccess] = useState(false);
+
+    /* valid email regex pattern */
+    const emailRegex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+
+    // const genericErrorStyles = ;
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+        setNameError(false);
+        setGenericErrorMsg(false);
+    };
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+        setEmailError(false);
+        setGenericErrorMsg(false);
+    };
+    const handleMessageChange = (event) => {
+        setMessage(event.target.value);
+        setMessageError(false);
+        setGenericErrorMsg(false);
+    };
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+
+        /* checking the value of the fields and updatng the state accordingly */
+        if (!name) setNameError(true);
+        if (!email) setEmailError(true);
+        if (emailRegex.test(email) === false) setEmailError(true);
+        if (!message) setMessageError(true);
+
+        if ((name === '') || (email === '') || (emailRegex.test(email) === false) || (message === '')) {
+            setGenericErrorMsg(true);
+        }
+
+        if (!nameError && !emailError && emailRegex.test(email) && !messageError) setSuccess(true);
+
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
+
+    useEffect(() => {
+        if (success) {
+            setTimeout(() => setSuccess(false), 2500);
+        }
+    });
+
     return (
         <div className='w-full px-6 py-3 text-raisin-black'>
             <div className='flex flex-col items-center justify-center max-w-4xl mx-auto mt-2 mb-6 md:mb- md:flex-row'>
                 <div className='flex flex-col items-start justify-center w-full mb-4 md:mr-8 md:mb-0'>
 
-                        <div className='mb-3'>
-                            <h1 className='text-xl font-bold uppercase md:text-2xl'>Hello, do you want to talk?</h1>
-                            <p className="my-2 font-semibold">If you want to contact to me, offer an advertisement or a collaboration, you can write me here</p>
-                        </div>
+                    <div className='mb-3'>
+                        <h1 className='text-xl font-bold uppercase md:text-2xl'>Hello, do you want to talk?</h1>
+                        <p className="my-2 font-semibold">If you want to contact me, offer an advertisement or a collaboration, you can write me here</p>
+                    </div>
 
-                        <div className="flex flex-col items-start justify-center w-full px-4 py-3 mb-3 border-2 border-dashed md:mb-2 bg-light-brown border-dashed-border h-fit">
-                            <h2 className='w-full mb-2 text-lg font-bold uppercase'>send me a message</h2>
-                           
-                            <form className='grid w-full grid-cols-2 grid-rows-5 mb-3 md:mb-5 gap-x-3 gap-y-3'>
-                                <input 
-                                className='col-span-2 row-span-1 p-2 border border-gray-400 md:col-span-1 bg-lavender focus:outline-dark-green' 
-                                type='text' 
-                                name='name' 
-                                id='name'
-                                placeholder='name' />
+                    <div className="flex flex-col items-start justify-center w-full px-4 py-3 mb-3 border-2 border-dashed md:mb-2 bg-light-brown border-dashed-border h-fit">
+                        <h2 className='w-full mb-2 text-lg font-bold uppercase'>send me a message</h2>
 
-                                <input 
-                                className='col-span-2 row-span-1 p-2 border border-gray-400 md:col-span-1 bg-lavender focus:outline-dark-green ' 
-                                type='email' 
-                                name='email' 
-                                id='email' 
-                                placeholder='email'/>
+                        {/* form validation generic error message*/}
+                        <p className={`mb-2 text-sm font-semibold text-red-500 ${genericErrorMsg ? 'block' : 'hidden'}`}>
+                            One or more field/s is empty, please enter the correct data
+                        </p>
 
-                                <textarea 
-                                className='col-span-2 row-span-3 p-2 border border-gray-400 bg-lavender focus:outline-dark-green ' 
-                                name="message" 
-                                id="message" 
-                                cols="20" 
-                                rows="5"
-                                placeholder='message'
+                        {/* form submited successfully message*/}
+                        <p className={`mb-2 text-sm font-semibold items-center justify-center text-green-500 ${success ? 'flex' : 'hidden'}`}>
+                            <MdOutlineMarkEmailRead className='w-8 h-8 md:mr-1' />Submited successfully, thanks for dropping by.
+                        </p>
+
+                        <form className='grid w-full grid-cols-2 grid-rows-5 mb-3 md:mb-5 gap-x-3 gap-y-3'>
+                            {/* name field */}
+                            <div
+                                className={`col-span-2 row-span-1 border shadow-inner focus-within:border-none md:col-span-1 ${nameError ? 'border-red-500' : 'border-gray-400'}`}
+                            >
+                                <label className='sr-only' htmlFor="name"></label>
+                                <input
+                                    className='w-full h-full p-2 bg-lavender focus:outline-dark-green'
+                                    onChange={handleNameChange}
+                                    value={name}
+                                    type='text'
+                                    id='name'
+                                    placeholder='name' />
+                            </div>
+
+                            {/* email field*/}
+                            <div
+                                className={`col-span-2 row-span-1 border shadow-inner focus-within:border-none md:col-span-1 ${emailError ? 'border-red-500' : 'border-gray-400'}`}
+                            >
+                                <label className='sr-only' htmlFor="email"></label>
+                                <input
+                                    className='w-full h-full p-2 bg-lavender focus:outline-dark-green'
+                                    onChange={handleEmailChange}
+                                    value={email}
+                                    type='email'
+                                    name='email'
+                                    id='email'
+                                    placeholder='email' />
+                            </div>
+
+                            {/* message field */}
+                            <div
+                                className={`col-span-2 row-span-3  border shadow-inner focus-within:border-none ${messageError ? 'border-red-500' : 'border-gray-400'}`}
+                            >
+                                <label className='sr-only' htmlFor="message"></label>
+                                <textarea
+                                    className={`w-full h-full p-2 border resize-none bg-lavender focus:outline-dark-green `}
+                                    onChange={handleMessageChange}
+                                    value={message}
+                                    name="message"
+                                    id="message"
+                                    placeholder='message'
                                 >
                                 </textarea>
+                            </div>
 
-                                <button 
-                                className='uppercase col-span-1 row-span-1 px-2 py-0.5 font-bold bg-dark-green text-lavender'
-                                type="submit">send</button>
-                            </form>
+                            {/* submit button */}
+                            <button
+                                className='uppercase col-span-1 transition duration-300 hover:text-raisin-black hover:cursor-pointer row-span-1 px-2 py-0.5 font-bold bg-dark-green text-lavender'
+                                type="submit"
+                                onClick={(event) => formSubmitHandler(event)}
+                            > send</button>
+                        </form>
 
-                            <SocialMedia author={author} />
-                        </div>
+                        <SocialMedia author={author} />
+                    </div>
 
                 </div>
-                <div className='relative h-96 w-full md:max-w-sm aspect-[16/9]'>
+                <div className='relative hidden md:block h-96 w-full md:max-w-sm aspect-[16/9]'>
                     <Image
                         src={flowerPhoto}
                         alt='flower photo'
@@ -71,11 +164,11 @@ const contact = ({author}) => {
 export default contact;
 
 
-export async function getStaticProps(){
-  const author = (await getAuthour() || []);
+export async function getStaticProps() {
+    const author = (await getAuthour() || []);
 
-  return{
-    props: {author}
-  }
+    return {
+        props: { author }
+    };
 
 }
