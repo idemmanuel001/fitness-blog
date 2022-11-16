861c749633fc6aa3332a0be8f5a526cc9af6a43a
idemmanuel001 <idemmanuel001@gmail.com>
2022-11-16 09:16:32 +0100

    fixed redirect issues

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -15,59 +15,61 @@ const Post = ({ post, similarPosts }) => {
         </div>
     );
 
-    const { title, image, content, tagsCollection, categgories } = post[0];
+    if (post && similarPosts) {
+        const { title, image, content, tagsCollection, categgories } = post[0];
 
-    return (
+        return (
 
-        <div className='w-full px-6 py-3 text-raisin-black'>
-            <div className='w-full max-w-4xl mx-auto'>
-                <h1 className='my-3 text-xl font-bold md:mb-6 md:text-4xl'>{title}</h1>
+            <div className='blog-post w-full px-6 py-3 text-raisin-black'>
+                <div className='w-full max-w-4xl mx-auto'>
+                    <h1 className='my-3 text-xl font-bold md:mb-6 md:text-4xl'>{title}</h1>
 
-                <div className='relative md:float-left block aspect-[16/9] md:h-60 md:shadow-sm md:mr-5  '>
-                    <Image
-                        src={image.url}
-                        alt={image.title}
-                        layout='fill'
-                        quality={30}
-                        objectFit='cover'
-                        objectPosition='center center'
-                    />
-                </div>
+                    <div className='relative md:float-left block aspect-[16/9] md:h-60 md:shadow-sm md:mr-5  '>
+                        <Image
+                            src={image.url}
+                            alt={image.title}
+                            layout='fill'
+                            quality={30}
+                            objectFit='cover'
+                            objectPosition='center center'
+                        />
+                    </div>
 
-                <div className='my-4 md:my-0 '>
-                    {documentToReactComponents(content.json)}
-                </div>
+                    <div className='my-4 md:my-0 '>
+                        {documentToReactComponents(content.json)}
+                    </div>
 
-                <ul className='mb-3 md:my-2'>
-                    {tagsCollection.items.map(tag => {
-                        return (
-                            <Link key={tag.tags} href={`/tags/${tag.tags}`} passHref>
-                                <span className="text-center cursor-pointer text-lavender bg-dark-green font-semibold rounded-sm py-0.5 px-1 mr-1 mb-1 text-sm align-middle ">
-                                    #{tag.tags}
-                                </span>
-                            </Link>
-                        );
-                    })}
-                </ul>
-
-                {/* Similar posts */}
-                <div className='my-6'>
-                    <h2 className='text-xl font-bold'>
-                        You Can Also Read
-                    </h2>
-
-                    <div className='flex flex-col items-center md:justify-start' >
-                        {similarPosts.map(post => {
+                    <ul className='mb-3 md:my-2'>
+                        {tagsCollection.items.map(tag => {
                             return (
-                                <PostPreview key={post.slug} post={post} />
+                                <Link key={tag.tags} href={`/tags/${tag.tags}`} passHref>
+                                    <span className="text-center cursor-pointer text-lavender bg-dark-green font-semibold rounded-sm py-0.5 px-1 mr-1 mb-1 text-sm align-middle ">
+                                        #{tag.tags}
+                                    </span>
+                                </Link>
                             );
                         })}
+                    </ul>
+
+                    {/* Similar posts */}
+                    <div className='my-6'>
+                        <h2 className='text-xl font-bold'>
+                            You Can Also Read
+                        </h2>
+
+                        <div className='flex flex-col items-center md:justify-start' >
+                            {similarPosts.map(post => {
+                                return (
+                                    <PostPreview key={post.slug} post={post} />
+                                );
+                            })}
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        </div>
-    );
+            </div>
+        );
+    }
 };
 
 export default Post;
@@ -78,7 +80,7 @@ export async function getStaticProps({ params }) {
     const similarPosts = (await getSimilarPosts(postCategory, params.slug) || []);
 
     /* redirecting back to homepage if the path does not exist */
-    if (!post && !similarPosts && !postCategory) {
+    if (!post.length || !postCategory.length) {
         return {
             redirect: {
                 destination: '/',
