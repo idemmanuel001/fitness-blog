f38d0017a72cd8dec3c7db28d78bbf368a048e2e
idemmanuel001 <idemmanuel001@gmail.com>
2022-12-21 00:10:46 +0100

    updated postList to showing mesage for categories with no posts

--- components/PostList.jsx
+++ components/PostList.jsx
@@ -21,19 +21,32 @@ const PostLists = ({ posts, categories }) => {
     setRenderedPosts(newPosts);
   };
 
+
+  if (renderedPosts.length < 1) {
+    return (
+      <div className='flex flex-col w-full md:grow md:mr-12 md:items-start md:justify-center'>
+        <Categories filterItems={filterItems} categories={categories} renderedPosts={renderedPosts} />
+
+        <h2 className='my-4 text-lg font-bold text-center text-night-rider'>No Post For This Category</h2>
+      </div>
+    );
+  }
+
+
   return (
     <div className='flex flex-col w-full md:grow md:mr-12 md:items-start md:justify-center'>
 
       <Categories filterItems={filterItems} categories={categories} renderedPosts={renderedPosts} />
 
-      {renderedPosts.map(post => {
+      {renderedPosts.length > 0 &&
+        renderedPosts.map(post => {
+          return (
+            <Fragment key={post.slug}>
+              <PostPreview post={post} />
+            </Fragment>
+          );
+        })}
 
-        return (
-          <Fragment key={post.slug}>
-            <PostPreview post={post} />
-          </Fragment>
-        );
-      })}
     </div>
   );
 
